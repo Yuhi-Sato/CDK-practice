b9da2443ee8df64036b118cbfaa054eed3018993
yuhisato <yuhi120101@gmail.com>
2023-06-24 09:20:36 +0900

    #1

--- lib/devio-stack.ts
+++ lib/devio-stack.ts
@@ -1,16 +1,10 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from '@aws-cdk/core';
+import { Vpc } from '@aws-cdk/aws-ec2'; // <- 追加
 
 export class DevioStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'DevioQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new Vpc(this, 'Vpc'); // <- 追加
   }
 }
