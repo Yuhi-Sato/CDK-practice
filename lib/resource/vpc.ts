a8caf1f5b68c9d7b0532a2ca442b3742869d5c8c
yuhisato <yuhi120101@gmail.com>
2023-06-25 10:43:10 +0900

    #8

--- lib/resource/vpc.ts
+++ lib/resource/vpc.ts
@@ -1,10 +1,13 @@
 import { Construct } from 'constructs';
 import { CfnVPC } from 'aws-cdk-lib/aws-ec2';
+import { Resource } from './abstract/resource';
 
-export class Vpc {
+export class Vpc extends Resource {
     public vpc: CfnVPC;
 
-    constructor() { };
+    constructor() {
+        super();
+     }
 
     public createResources(scope: Construct) {
         const systemName = scope.node.tryGetContext('systemName');
